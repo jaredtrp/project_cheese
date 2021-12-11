620b3c5d1e3c484d40c828af2e7df9268a5e593e
GunnySensei <gunnerwagoner@hotmail.com>
2021-12-11 16:59:50 -0600

    configured jquery in printcheesecards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var container = $("#container");
+
 const cheeses = [
     {
         name: "Havarti",
@@ -29,15 +31,23 @@ const cheeses = [
 //print the cards of cheese on the page at open
 var printCheeseCards = function() {
     for(var i = 0; i < cheeses.length; i++) {
-        var taskdiv = $("<div>").append("#container");
-        var image = $("<img/>").append("#container");
+        console.log("card# " + cheeses[i]);
+        var taskdiv = $("<div>").addClass("taskdiv")
+            // .append("#container");
+        var image = $("<img>").addClass("image");
+            // .append("#container");
         var cheeseName = $("<h2>")
-            .text(cheeses[i].name)
-            .append("#container");
-    }
-}
+            .addClass("cheese-name")
+            .text(cheeses[i].name);
+            // .append("#container");
+        container.append(taskdiv);
+        container.append(image);
+        container.append(cheeseName);
+    };      
+            
+};
 // var createTask = function(taskText, taskDate, taskList) {
-//     // create elements that make up a task item
+//     create elements that make up a task item
 //     var taskLi = $("<li>").addClass("list-group-item");
 //     var taskSpan = $("<span>")
 //       .addClass("badge badge-primary badge-pill")
@@ -46,50 +56,16 @@ var printCheeseCards = function() {
 //       .addClass("m-1")
 //       .text(taskText);
   
-//     // append span and p element to parent li
+//     append span and p element to parent li
 //     taskLi.append(taskSpan, taskP);
   
-//     // check due date
+//     check due date
 //     auditTask(taskLi);
   
-//     // append to ul list on the page
+//     append to ul list on the page
 //     $("#list-" + taskList).append(taskLi);
 //   };
+
+
 // call the printcheesecards function
 printCheeseCards();
-
-//     for(var i = 0; i < timeSlots.length; i++) {
-//         var containerSections = document.createElement("div");
-//         containerSections.classList = "row d-flex flex-row";
-//         containerSections.id = "containerSections";
-//         containerEl.appendChild(containerSections);
-
-//         var blockEl = document.createElement('div');
-//         blockEl.classList = "time-block col-1";
-//         containerSections.appendChild(blockEl);
-
-//         var timeInSlot = document.createElement("p");
-//         timeInSlot.id = "timeInSlot"  + timeSlots.indexOf();
-//         timeInSlot.classList = "hour";
-//         timeInSlot.textContent = timeSlots[i];
-//         blockEl.appendChild(timeInSlot);
-
-//         var eventWritingSection = document.createElement("p");
-//         eventWritingSection.id = "eventWritingSection"  + timeSlots.indexOf();
-//         eventWritingSection.textContent = " ";
-//         eventWritingSection.classList = "past col-9";
-//         containerSections.appendChild(eventWritingSection);
-
-//         var saveButtonSlot = document.createElement("p");
-//         saveButtonSlot.id = "saveButtonSlot" + timeSlots.indexOf();
-//         saveButtonSlot.classList = "saveBtn col-1";
-//         containerSections.appendChild(saveButtonSlot);
-
-//         var saveButton = document.createElement("button");
-//         saveButton.id = "saveButton" + timeSlots.indexOf();
-//         saveButton.classList = "saveBtnKiddo";
-//         saveButton.innerHTML = "<span class='glyphicon glyphicon-floppy-save'></span>"
-//         saveButtonSlot.appendChild(saveButton);
-//     }
-    
-// }
